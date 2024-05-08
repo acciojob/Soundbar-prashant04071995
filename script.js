f6213c5ff92dd527d691a01fed5bb4d8a8a199c6
Prashant Salve <128687203+prashant04071995@users.noreply.github.com>
2024-05-08 13:41:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let audio = null;
+
+function playSound(fileName) {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+    audio = new Audio(`sounds/${fileName}`);
+    audio.play();
+}
+
+function stopSound() {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
